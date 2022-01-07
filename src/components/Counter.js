c6b9506e0f30c06c8ae1406f654eef33d56fbc45
Aditya <adiadi7898@gmail.com>
2022-01-07 14:05:24 +0530

    Fixed an error

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,67 +2,64 @@ import React from 'react'
 
 export default function Counter() {
     let n = 0;
-    let counter = document.getElementById('count');
 
-    const CounterDecrease = () => {
-        console.log('Decrease was clicked');
+    function OnDecrease() {
+        let counter = document.getElementById('count');
+        console.log("Decrease was clicked");
         n--;
         counter.textContent = n;
         if (counter.textContent < 0) {
             counter.style.color = 'Red';
         }
+        else if (counter.textContent === 0) {
+            counter.style.color = 'Black';
+        }
         else if (counter.textContent > 0) {
             counter.style.color = 'Green';
         }
-        else {
-            counter.style.color = 'black';
-        }
     }
 
-    const CounterIncrease = () => {
-        console.log('Increase was clicked');
+    function OnReset() {
+        let counter = document.getElementById('count');
+        console.log("Reset was clicked");
+        n = 0;
+        counter.textContent = n;
+        counter.style.color = 'black';
+    }
+
+    function OnIncrease() {
+        let counter = document.getElementById('count');
+        console.log("Increase was clicked");
         n++;
         counter.textContent = n;
         if (counter.textContent < 0) {
             counter.style.color = 'Red';
         }
+        else if (counter.textContent === 0) {
+            counter.style.color = 'Black';
+        }
         else if (counter.textContent > 0) {
             counter.style.color = 'Green';
         }
-        else {
-            counter.style.color = 'black';
-        }
     }
 
-    const CounterReset = () => {
-        console.log('Reset was clicked');
-        counter.style.color = 'black';
-        n = 0;
-        counter.textContent = n;
-    }
-
-
-
-
-
-return (
-    <div>
-        <div className='Container'>
-            <h1>
-                Counter:
-                <div id='count'>
-                    0
+    return (
+        <div>
+            <div className='Container'>
+                <h1>
+                    Counter:
+                    <div id='count'>
+                        0
+                    </div>
+                </h1>
+                <div>
+                    <span className='buttons'>
+                        <button className='Decrease' onClick={OnDecrease}>Decrease</button>
+                        <button className='Reset' onClick={OnReset}>Reset</button>
+                        <button className='Increase' onClick={OnIncrease}>Increase</button>
+                    </span>
                 </div>
-            </h1>
-            <div>
-                <span className='buttons
-                    '>
-                    <button className='Decrease' onClick={CounterDecrease}>Decrease</button>
-                    <button className='Reset' onClick={CounterReset}>Reset</button>
-                    <button className='Increase' onClick={CounterIncrease}>Increase</button>
-                </span>
             </div>
         </div>
-    </div>
-)
+    )
 }
